a7c289ee76802f8e0f11dfe69d3dcb5d57835e6b
桑原龍樹 <tatsuki.kuwahara@vitalize.co.jp>
2020-12-28 20:50:47 +0900

    usersテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,43 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "username", null: false
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "password_salt"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "auth_token"
+    t.boolean "use_app"
+    t.datetime "app_used_at"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "sms_sent_at"
+    t.string "sms_code_token"
+    t.string "provider"
+    t.string "uid"
+    t.integer "creator_id"
+    t.integer "updater_id"
+    t.integer "deleter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.index ["confirmation_token"], name: "usrs_on_confirmation_token", unique: true
+    t.index ["email"], name: "usrs_on_email", unique: true
+    t.index ["reset_password_token"], name: "usrs_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "usrs_on_unlock_token", unique: true
+  end
+
 end
